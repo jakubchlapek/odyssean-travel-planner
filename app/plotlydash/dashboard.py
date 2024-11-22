4701ea7d287c965fafcefce22e6fd360c383504c
jakubchlapek <jakub.chlapekk@gmail.com>
2024-11-22 01:11:15 +0100

    Added basic plots

--- app/plotlydash/dashboard.py
+++ app/plotlydash/dashboard.py
@@ -1,37 +1,56 @@
-import dash
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 from flask import Flask
-from app.plotlydash.data import fetch_data
+from app.plotlydash.data import fetch_data, get_trip_info
 import numpy as np
 import pandas as pd
-
 
 def init_dash_app(server):
     # Initialize Dash app with the parent Flask app 
     # With this setup Dash piggybacks off of the Flask server as a module, opposed to running as a standalone server
     server.logger.info("Initializing Dash app.")
-    dash_app = dash.Dash(
+    dash_app = Dash(
         server=server,
         routes_pathname_prefix='/dash/',
         external_stylesheets=[
-            '/static/dist/css/styles.css',
+            '/static/css/graphs.css',
         ]
     )
+    
+    dash_app.layout = html.Div(id='dash-container', children=[
+        dcc.Location(id="url", refresh=False),
+        dcc.Store(id="data-store"),  # Store to hold fetched data
+        html.H1(id="trip-title", children=[]),
+        html.Div(id='budget-graphs-box', children=[
+            dcc.Graph(id='budget-bar-graph'), 
+            dcc.Graph(id='budget-pie-graph')
+        ]),
+        html.Div(id="graph-buttons", children=[
+            html.Label('Choose categories'),
+            dcc.Dropdown(
+                options=[
+                    {'label': 'Accommodation', 'value': 'Accommodation'},
+                    {'label': 'Food', 'value': 'Food'},
+                    {'label': 'Transport', 'value': 'Transport'},
+                    {'label': 'Entertainment', 'value': 'Entertainment'},
+                    {'label': 'Other', 'value': 'Other'}
+                ],                
+                value=['Accommodation', 'Food', 'Transport', 'Entertainment', 'Other'],
+                id="dropdown-categories",
+                multi=True,
+            )
+        ])
 
-    dash_app.layout = dash.html.Div(id='dash-container', children=[
-        dash.dcc.Location(id="url", refresh=False),
-        dash.dcc.Store(id="data-store"),  # Store to hold fetched data
-        dash.dcc.Graph(id='budget-graph') 
     ])
     
     init_callbacks(dash_app)
 
     return dash_app.server
     
-def init_callbacks(dash_app):
+def init_callbacks(dash_app):    
     @dash_app.callback(
-    dash.Output("data-store", "data"),
-    dash.Input("url", "pathname")
+    Output("data-store", "data"),
+    Input("url", "pathname")
     )
     def load_data(pathname):
         try:
@@ -43,38 +62,148 @@ def init_callbacks(dash_app):
         return data
     
     @dash_app.callback(
-        dash.Output("budget-graph", "figure"),
-        dash.Input("data-store", "data")
+        Output("trip-title", "children"),
+        Input("url", "pathname")
     )
-    def update_graph(data):
+    def get_title(pathname):
+        try:
+            trip_id = int(pathname.split("/")[-1])  # Attempt to extract trip ID
+        except (ValueError, IndexError):
+            return None
+        
+        trip_name, trip_cost, preferred_currency = get_trip_info(trip_id)
+        return f"Trip: {trip_name} - total cost: {trip_cost} {preferred_currency}"
+    
+    @dash_app.callback(
+        Output("budget-bar-graph", "figure"),
+        Input("data-store", "data"),
+        Input("dropdown-categories", "value")
+    )
+    def add_bar_graph(data, chosen_categories):
         if data:
-            # Convert data into a pandas DataFrame
-            df = pd.DataFrame(data)
+            df = pd.DataFrame(data[0])
+            preferred_currency = data[1]
 
-            # Check if required columns exist
-            required_columns = ["base_cost", "component_name", "exchange_rate", "category_name", "title"]
-            if not all(col in df.columns for col in required_columns[:-1]):  # Exclude "title" as it's not a column
-                raise ValueError(f"Missing one or more required columns: {required_columns[:-1]}")
+            required_columns = ["base_cost", "component_name", "exchange_rate", "category_name"]
+            if not all(col in df.columns for col in required_columns):
+                raise ValueError(f"Missing one or more required columns: {required_columns}")
 
             # Filter rows where base_cost > 0
             df = df[df["base_cost"] > 0]
+            
+            # Filter by chosen_categories
+            df = df[df["category_name"].isin(chosen_categories)]
 
             if df.empty:
                 return px.line(title="No valid data to display.")
 
-            # Calculate adjusted cost
             df["adjusted_cost"] = df["base_cost"] * df["exchange_rate"]
 
-            # Create a bar plot
             fig = px.bar(
                 data_frame=df,
                 x="component_name",
                 y="adjusted_cost",
-                title=data["title"],
+                title= f"",
                 labels={"component_name": "Component", "adjusted_cost": "Cost"},
-                color="category_name"
+                color="category_name",  
+                color_discrete_map={
+                    'Accommodation': '#EA4848', 
+                    'Food': '#4FB477', 
+                    'Transport': '#85CCFF',
+                    'Entertainment': '#A975A4',
+                    'Shopping': '#FF9B42',
+                    'Other': '#B8B8B8'
+                },
+                hover_data={"start_date": True, "end_date": True, "link": True, "category_name": False},
+                hover_name="description",
+                custom_data=["start_date", "end_date", "link"],
+                width=365,
+                height=365
+            )
+            fig.update_layout(
+                hoverlabel=dict(
+                    bgcolor="white",
+                    font_size=12,
+                    font_family="Fira Sans",
+                ),
+                paper_bgcolor="#f3ebdf",
+                plot_bgcolor="#f3ebdf",
+                barmode="relative",
+                xaxis_title="Component",
+                yaxis_title=f"Cost ({preferred_currency})",
+                showlegend=True,
+                legend_title="Category",       
+                margin=dict(l=30, r=0, t=40, b=30),
+                font_family="Fira Sans",     
+            )
+            fig.update_traces(
+                hovertemplate="<b>%{x}</b><br><br>" +
+                              "Cost: %{y:.2f}" + f"{preferred_currency}<br>" +
+                              "Description: %{hovertext}<br>" +
+                              "Start date: %{customdata[0]}<br>" +
+                              "End date: %{customdata[1]}<br>" +
+                              "Link: %{customdata[2]}<extra></extra>",
+                marker=dict(line=dict(width=1, color="Black")),
+                               
             )
             return fig
         # Placeholder figure if no data is loaded yet
-        return px.line(title="Waiting for data...")
+        return px.line(title="Waiting for data...", height=365, width=365)    
     
+    @dash_app.callback(
+        Output("budget-pie-graph", "figure"),
+        Input("data-store", "data"),
+        Input("dropdown-categories", "value")
+    )
+    def add_pie_graph(data, chosen_categories):
+        if data:
+            df = pd.DataFrame(data[0])
+            preferred_currency = data[1]
+
+            required_columns = ["base_cost", "component_name", "exchange_rate", "category_name"]
+            if not all(col in df.columns for col in required_columns):  # Exclude "title" and "preferred_currency" as it's not a column
+                raise ValueError(f"Missing one or more required columns: {required_columns}")
+            
+            # Filter by chosen_categories
+            df = df[df["category_name"].isin(chosen_categories)]
+            
+            fig = px.pie(
+                data_frame=df,
+                values="base_cost",
+                names="component_name",
+                color="category_name",  
+                color_discrete_map={
+                    'Accommodation': '#EA4848', 
+                    'Food': '#4FB477', 
+                    'Transport': '#85CCFF',
+                    'Entertainment': '#A975A4',
+                    'Shopping': '#FF9B42',
+                    'Other': '#B8B8B8'
+                },
+                width=365,
+                height=365,
+                custom_data=["category_name", "type_name"],
+            )
+            fig.update_layout(
+                hoverlabel=dict(
+                    bgcolor="white",
+                    font_size=12,
+                    font_family="Fira Sans",
+                ),
+                margin=dict(l=0, r=30, t=40, b=30),
+                paper_bgcolor="#f3ebdf",
+                plot_bgcolor="#f3ebdf",
+                legend_title="Component name",
+                font_family="Fira Sans",     
+            )
+            fig.update_traces(
+                textposition="inside",
+                textinfo='percent+label',
+                hovertemplate="<b>%{name}</b><br><br>" +
+                    "Cost: %{value:.2f}" + f"{preferred_currency}<br>" +
+                    "Category: %{customdata[0]}<br>" +
+                    "Subcategory: %{customdata[1]}<extra><extra>",
+            )
+            return fig
+        # Placeholder figure if no data is loaded yet
+        return px.line(title="Waiting for data...", height=365, width=365)
--- app/plotlydash/data.py
+++ app/plotlydash/data.py
@@ -7,6 +7,15 @@ import sqlalchemy as sa
 category_names = {i + 1: category for i, category in enumerate(Config.INIT_CATEGORIES)}
 type_names = {i + 1: type_name for i, type_name in enumerate({type_ for types in Config.INIT_TYPES.values() for type_ in types})}
 
+def get_trip_info(trip_id: int):
+    """Fetch trip name, cost and currency, return a tuple (name, cost)"""
+    app.logger.info(f"Fetching name and cost for trip id: {trip_id}.")
+    if not trip_id or not isinstance(trip_id, int):
+        return None
+    trip = db.first_or_404(sa.select(Trip).where(Trip.id == trip_id))
+    preferred_currency = trip.user.preferred_currency if trip.user else "PLN"
+    return (trip.trip_name, trip.get_total_cost(), preferred_currency)
+
 
 def fetch_data(trip_id: int):
     """Fetch components list and trip name from the database, run it to create_dataframe and return it."""
@@ -16,29 +25,32 @@ def fetch_data(trip_id: int):
     trip = db.first_or_404(sa.select(Trip).where(Trip.id == trip_id))
     preferred_currency = trip.user.preferred_currency if trip.user else "PLN"
     components = db.session.scalars(trip.components.select()).all()
-    data = data_to_dict(components, trip.trip_name, trip.get_total_cost(), preferred_currency)
+    data = data_to_dict(components, preferred_currency)
     return data
 
 
-def data_to_dict(components: list[Component], trip_name: str, trip_total_cost: float, preferred_currency: str) -> dict:
-    """Create a dictionary created from a list of components."""
-    app.logger.info(f"Creating dictionary from components list for trip: {trip_name}.")
+def data_to_dict(components: list[Component],  preferred_currency: str) -> dict:
+    """Create a dictionary created from a list of components.
+    
+    Returns:
+        data: Dictionary with trip data.
+        preferred_currency: Preferred currency of the user.
+    """
     if not components:
         return dict()
+    app.logger.info(f"Creating dictionary from components list for trip id: {components[0].trip_id}.")
     
     data = { # Dictionary with default values for missing data
         "component_name": [getattr(c, 'component_name', None) for c in components],
         "category_name": [category_names.get(c.category_id, "Unknown Category") for c in components],
-        "subcategory_name": [type_names.get(c.type_id, "Unknown Subcategory") for c in components],
+        "type_name": [type_names.get(c.type_id, "Unknown Subcategory") for c in components],
         "base_cost": [getattr(c, 'base_cost', 0.0) for c in components],
         "link": [getattr(c, 'link', None) for c in components], 
         "description": [getattr(c, 'description', "") for c in components],
         "start_date": [getattr(c, 'start_date', pd.NaT) for c in components], 
         "end_date": [getattr(c, 'end_date', pd.NaT) for c in components],      
-        "currency": [getattr(c, 'currency', "PLN") for c in components],
+        "original_currency": [getattr(c, 'currency', "PLN") for c in components],
         "exchange_rate": [get_exchange_rate(c.currency, preferred_currency) for c in components],
-        "total_cost": trip_total_cost,     
-        "title": trip_name
     }
     
-    return data
+    return (data, preferred_currency)
